82735978b163004c0442c95c1a3ea7c99cde6e1d
maz <complex.logik@gmail.com>
2018-03-22 17:40:04 -0400

    updating where im at

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 
 //scroll to bottom
 
-document.getElementById("scrollToBottom").addEventListener("click", function () {
-  document.body.scrollIntoView(false);
-});
 
-$(".hover").mouseleave(
-  function () {
-    $(this).removeClass("hover");
-  }
-);
+document.getElementById("scrollToBottom").addEventListener("click",(event)  => {
+  document.body.scrollIntoView(false);
+};
